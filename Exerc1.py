881e72e1c1ce3576d62c922c3844e74f4915aa74
AirtonDias <airton-feitosa@outlook.com>
2025-03-29 16:11:24 -0300

    Exerc1 finalizado

--- Exerc1.py
+++ Exerc1.py
@@ -1,53 +1,59 @@
 repeticoes = int(input())
 
-contagem = 6  ##contador 2º while
-cont = 0  ##contador 1º while
-listEntrada = []
-localMaiorRep = [0,0,0]
+contagem = 6                ##contador 2º while
+cont = 0                    ##contador 1º while
+localMaiorRep = [0,0,0]     #contagem de qual linha é a mais usada
+regioes = [0,0,0]           #primeiro indice é linha 1 e 2
+                            #segundo indice é linha 3 e 4
+                            #Tercerio indice é linha 5 e 6
+
 
 while cont != repeticoes:
-    
-    while contagem > 0:
-        
+    localMaiorRep = [0,0,0]     #contagem de qual linha é a mais usada
+    listEntrada = []
+
+    while contagem > 0:         ###Entradas das 6 linhas
         entrada = input()
         listEntrada.append(entrada.split(sep = " "))
-        
         contagem = contagem - 1
         
-    soma = 0
     linha = 0
+    localMaiorRep = [0,0,0]     #lista das linhas + usadas
     
     for x in listEntrada:
         soma = 0
+        for j in x:
+            soma = soma + int(j)
+
+        if linha < 2:
+            localMaiorRep[0] += soma
+        else:
+            if linha < 4:
+                localMaiorRep[1] += soma
+            else:
+                if linha < 6:
+                    localMaiorRep[2] += soma
+
         linha += 1
-        for j in listEntrada:
-            
-            soma = 0
-            soma = soma + j
+ 
+    contagem = 6        # reset da contagem de linha
+    cont = cont + 1     #proximo conjunto de numeros
+    
+    maior = max(localMaiorRep)
+    final = localMaiorRep.index(maior)
+    regioes[final] += 1
 
-        if linha <= 6:
-            if linha <= 4:
-                if linha <= 2:
-                    
-                    localMaiorRep[0] += 1
-                localMaiorRep[1] += 1
-            localMaiorRep[2] += 1
-                
-    
+print(localMaiorRep)
+print(regioes)
 
-    
-   # listEntrada = []
-    contagem = 6
-    cont = cont + 1
-    
-for x in listEntrada:
-    
-    print(x)
-    
-    
-  #      cont2 = 0, max = 0
-  #  
-   ## for i in listEntrada:
-        
-  #      max += i[cont2]
-   #     cont2 += 1
+maior = max(regioes)
+final = regioes.index(maior)
+
+if final == 2:
+    print("inferior")
+else:
+    if final == 1:
+        print("meio")
+    else:
+        if final == 0:
+            print("superior")
